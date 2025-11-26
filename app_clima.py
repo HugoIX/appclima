365a2b36446e36434a60f5dc30f4866c668afee7
Hugo Perez <hapm0423@hotmail.com>
2025-11-26 13:03:58 -0500

    Add files via upload

--- app_clima.py
+++ app_clima.py
@@ -1,32 +1,48 @@
 import requests
 
-ciudad = input("Ingrese una ciudad: ").title()
+while True: 
 
-fuente = "https://api.openweathermap.org/data/2.5/weather?q={}&lang=es&appid=16bb858c02f9b349a7b5f4f0077ebea9&units=metric".format(ciudad)
+    print("BIENVENIDO AL SISTEMA DE METEREOLOGÍA \n")
 
-res = requests.get(fuente)
+    ciudad = input("Ingrese una ciudad: ").title()
 
-data = res.json()
+    fuente = "https://api.openweathermap.org/data/2.5/weather?q={}&lang=es&appid=16bb858c02f9b349a7b5f4f0077ebea9&units=metric".format(ciudad)
 
-if res.status_code == 200:
+    res = requests.get(fuente)
+
     data = res.json()
 
-    temp = data["main"]["temp"]
-    temp_min = data["main"]["temp_min"]
-    temp_max = data["main"]["temp_max"]
-    rain = data.get("rain", {}.get("1h", 0))
-    clouds = data["clouds"]["all"]
+    if res.status_code == 200:
+        data = res.json()
 
-    description = data["weather"][0]["description"]
+        temp = data["main"]["temp"]
+        temp_min = data["main"]["temp_min"]
+        temp_max = data["main"]["temp_max"]
+        rain = data.get("rain", {}.get("1h", 0))
+        clouds = data["clouds"]["all"]
 
-    print(f"INFORME DEL CLIMA EN: {ciudad}")
-    print("Ciudad: ", data["name"])
-    print("Temperatura: ", temp, "°C")
-    print("Temperatura Mínima/Máxima: ", temp_min, "°C", "  /  ", temp_max, "°C")
-    print("Pronostico de lluvia (Última Hora): ", rain, "mm")
-    print("Nubosidad: ", clouds, "%")
-    print("Descripción: ", description.capitalize())
+        description = data["weather"][0]["description"]
 
-else:
-    print(f"Error al obtener los datos de la capa: {res.status_code}.")
-    print("Asegúrese de que la ciudad sea válida.")
+        print(f"INFORME DEL CLIMA EN: {ciudad}")
+        print("Ciudad: ", data["name"])
+        print("Temperatura: ", temp, "°C")
+        print("Temperatura Mínima/Máxima: ", temp_min, "°C", "  /  ", temp_max, "°C")
+        print("Pronostico de lluvia (Última Hora): ", rain, "mm")
+        print("Nubosidad: ", clouds, "%")
+        print("Descripción: ", description.capitalize())
+
+    else:
+        print(f"Error al obtener los datos de la capa: {res.status_code}.")
+        print("Asegúrese de que la ciudad sea válida.")
+
+    while True:
+        seguir = input("¿Desea consultar otra ciudad?(S/N): ").strip().lower()
+
+        if seguir == "s":
+            break
+        elif seguir == "n":
+            print("Gracias. Hasta Luego.")
+            exit()
+        else:
+            print("Opción Invalida. Presione 'S' o 'N'.")
+        
